8639f6884a24f2d1df854b4dc4bc9dc3bf35bf17
Travis-Findley <travis@findleyfamilies.com>
2023-01-03 11:12:06 -0600

    Changed Name

--- addingnumbers/Properties/AssemblyInfo.cs
+++ addingnumbers/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("addingnumbers")]
+[assembly: AssemblyTitle("AddingNumbers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("addingnumbers")]
+[assembly: AssemblyProduct("AddingNumbers")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
